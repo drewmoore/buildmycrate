de0f7fa986a65a8c3819a723bfe2ca8c77ccb821
Drew Moore <cantsnuffthedrewster@gmail.com>
2017-05-10 22:55:50 -0500

    First jest test passing.

--- app/react/components/search/__tests__/Form.js.jsx
+++ app/react/components/search/__tests__/Form.js.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React    from 'react';
 import renderer from 'react-test-renderer';
-import Form from '../Form.js.jsx';
+import jquery   from 'jquery';
+import Form     from '../Form.js.jsx';
 
-test('bpm_min should have a default value', () => {
-  const search    = { bpm_min: 120, bpm_max: 130, key_signature: 'cmaj' };
-  const component = renderer.create(<Form {...search} />);
+window.$ = jquery;
+
+test('should match snapshot', () => {
+  const props = {
+    bpm_min: 120, bpm_max: 130, key_signature: 'cmaj',
+    isFetching: false, isSpinning: false, submitForm: () => {}
+  };
+  const component = renderer.create(<Form {...props} />);
   const tree      = component.toJSON();
-
   expect(tree).toMatchSnapshot();
-  
 });
